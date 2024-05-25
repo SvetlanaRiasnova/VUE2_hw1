23da3ee35ab92da3321420843036913b8dcc499c
Svetlana Riasnova <R_S_U@outlook.com>
2024-05-25 16:07:52 +0300

    added  home wort 2

---
+++ VueHW2/Interno/script.js
@@ -0,0 +1,43 @@
+new Vue({
+    el: '.blog__articles',
+    data: {
+       articles: [
+          {
+             image: "art1.png",
+             itemHeader: "Kitchan Design",
+             itemTitle: "Let’s Get Solution For Building Construction Work",
+             itemData: "26 December,2022"
+          },
+          {
+             image: "art2.png",
+             itemHeader: "Living Design",
+             itemTitle: "Low Cost Latest Invented Interior Designing Ideas.",
+             itemData: "22 December,2022"
+          },
+          {
+             image: "art3.png",
+             itemHeader: "Interior Design",
+             itemTitle: "Best For Any Office & Business Interior Solution",
+             itemData: "25 December,2022"
+          },
+          {
+             image: "art4.png",
+             itemHeader: "Kitchan Design",
+             itemTitle: "Let’s Get Solution For Building Construction Work",
+             itemData: "26 December,2022"
+          },
+          {
+             image: "art5.png",
+             itemHeader: "Living Design",
+             itemTitle: "Low Cost Latest Invented Interior Designing Ideas.",
+             itemData: "22 December,2022"
+          },
+          {
+             image: "art6.png",
+             itemHeader: "Interior Design",
+             itemTitle: "Best For Any Office & Business Interior Solution",
+             itemData: "25 December,2022"
+          },
+       ]
+    }
+ })
